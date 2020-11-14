4b786872e133bd0616dde738bf1732cd95f7c8e2
Михаил Дорышин <doryshin19@yandex.ru>
2020-11-14 15:52:09 +0500

    Добавьте файлы проекта.

---
+++ DataAnalysisLab1/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace DataAnalysisLab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbInput = new System.Windows.Forms.RichTextBox();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbInput
+            // 
+            this.rtbInput.Location = new System.Drawing.Point(12, 12);
+            this.rtbInput.Name = "rtbInput";
+            this.rtbInput.Size = new System.Drawing.Size(776, 181);
+            this.rtbInput.TabIndex = 0;
+            this.rtbInput.Text = "";
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.rtbOutput.Location = new System.Drawing.Point(12, 257);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.ReadOnly = true;
+            this.rtbOutput.Size = new System.Drawing.Size(776, 181);
+            this.rtbOutput.TabIndex = 1;
+            this.rtbOutput.Text = "";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(329, 213);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(133, 24);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Преобразовать";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.rtbInput);
+            this.Name = "Form1";
+            this.Text = "App";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbInput;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ DataAnalysisLab1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace DataAnalysisLab1
+{
+    public partial class Form1 : Form
+    {
+
+        private static Dictionary<char, int> RomanMap = new Dictionary<char, int>()
+    {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+        Regex reg = new Regex(@"^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            rtbOutput.Text = "";
+            string[] text = rtbInput.Text.Split(' ');
+            for(int i = 0; i < text.Length; i++)
+            {
+                if (reg.IsMatch(text[i]))
+                {
+                    text[i] = RomanToInt(text[i]);
+                }
+                rtbOutput.Text += text[i] + " ";
+            }
+            rtbOutput.Text.Trim(' ');
+        }
+        private static string RomanToInt(string roman)
+        {
+            int num = 0;
+            for (int i = 0; i < roman.Length; i++)
+            {
+                if (i + 1 < roman.Length && RomanMap[roman[i]] < RomanMap[roman[i + 1]])
+                {
+                    num -= RomanMap[roman[i]];
+                }
+                else
+                {
+                    num += RomanMap[roman[i]];
+                }
+            }
+            return num.ToString();
+        }
+    }
+}
